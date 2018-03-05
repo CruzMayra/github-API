cf8eaeae3461f43134bdd238f31b018aeba53580
biokah <maria.anaisr@gmail.com>
2018-03-05 10:41:09 -0600

    limpiando input/section

--- public/app.js
+++ public/app.js
@@ -1,37 +1,40 @@
 
-const userArray = ['AdrixSC', 'superliza', 'CruzMayra', 'biokah']
-function iterateUsers(userArray){
-    for(var i = 0; i < userArray.length; i++){
-        getData(userArray[i])
-    }
-}
-
-iterateUsers(userArray)
+// const userArray = ['AdrixSC', 'superliza', 'CruzMayra', 'biokah', 'Hanie24']
+// function iterateUsers(userArray){
+//     for(var i = 0; i < userArray.length; i++){
+//         getData(userArray[i])
+//     }
+// }
 
 function getData (url){
-var request = new XMLHttpRequest();
-
-request.open('get', 'https://api.github.com/users/' + url)
-
-
-request.onload = getUser;
-request.onerror = handleError;
-request.send()
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://api.github.com/users/' + url)
+    request.onload = getUser;
+    request.onerror = handleError;
+    request.send()
 }
-
 
 function handleError(){
     console.log('se ha presentado un error');
 }
 
 function getUser(){
-    
     const data = JSON.parse(this.responseText)
-    console.log(data.avatar_url)
+    var pics = document.getElementById('pics');
+    pics.innerHTML = ''
+    //console.log(data)
     var photo = document.createElement('img');
     photo.setAttribute('src', data.avatar_url)
-    var pics = document.getElementById('pics');
     pics.appendChild(photo)
 }
 
-//getData()
+//iterateUsers(userArray)
+
+var button = document.getElementById('button-search');
+button.addEventListener('click', searchUser)
+
+function searchUser(){
+    var user = document.getElementById('input-user');
+    console.log(user)
+    getData(user.value)
+}

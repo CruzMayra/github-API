412bd1901333831291c3f12b03c2b7f193439661
superliza <eli_darko@hotmail.com>
2018-03-05 10:36:29 -0600

    se limpia contenedor e input

--- public/app.js
+++ public/app.js
@@ -1,37 +1,74 @@
 
-const userArray = ['AdrixSC', 'superliza', 'CruzMayra', 'biokah']
-function iterateUsers(userArray){
-    for(var i = 0; i < userArray.length; i++){
-        getData(userArray[i])
-    }
-}
-
-iterateUsers(userArray)
-
 function getData (url){
-var request = new XMLHttpRequest();
-
-request.open('get', 'https://api.github.com/users/' + url)
-
-
-request.onload = getUser;
-request.onerror = handleError;
-request.send()
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://api.github.com/users/' + url)
+    request.onload = getUser;
+    request.onerror = handleError;
+    request.send()
 }
-
 
 function handleError(){
     console.log('se ha presentado un error');
 }
 
 function getUser(){
-    
     const data = JSON.parse(this.responseText)
-    console.log(data.avatar_url)
+    // console.log(data)
+    var pics = document.getElementById('pics');
+    pics.innerHTML = "";
     var photo = document.createElement('img');
     photo.setAttribute('src', data.avatar_url)
-    var pics = document.getElementById('pics');
+    
     pics.appendChild(photo)
+    
 }
 
-//getData()
+//iterateUsers(userArray)
+
+var button = document.getElementById('button-search');
+button.addEventListener('click', searchUser)
+
+function searchUser(){
+    var user = document.getElementById('input-user');
+    // user.value = "";
+    getData(user.value);
+    user.value = "";
+}
+
+
+// const userArray = ['AdrixSC', 'superliza', 'CruzMayra', 'biokah']
+// function iterateUsers(userArray){
+//     for(var i = 0; i < userArray.length; i++){
+//         getData(userArray[i])
+//     }
+// }
+
+// iterateUsers(userArray)
+
+// function getData (url){
+// var request = new XMLHttpRequest();
+
+// request.open('get', 'https://api.github.com/users/' + url)
+
+
+// request.onload = getUser;
+// request.onerror = handleError;
+// request.send()
+// }
+
+
+// function handleError(){
+//     console.log('se ha presentado un error');
+// }
+
+// function getUser(){
+    
+//     const data = JSON.parse(this.responseText)
+//     console.log(data.avatar_url)
+//     var photo = document.createElement('img');
+//     photo.setAttribute('src', data.avatar_url)
+//     var pics = document.getElementById('pics');
+//     pics.appendChild(photo)
+// }
+
+// //getData()
